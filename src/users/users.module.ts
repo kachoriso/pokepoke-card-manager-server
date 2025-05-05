271412db7f1cf823049f5e1f957b6d29e927a37d
kachoriso <32869840+kachoriso@users.noreply.github.com>
2025-05-05 08:11:01 +0000

    fix userAll

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,22 +1,12 @@
-// packages/server/src/users/users.module.ts
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-// import { UsersController } from './users.controller'; // もし今後ユーザー情報取得などのAPIを作る場合は追加
-import { PrismaModule } from '../prisma/prisma.module'; // UsersService が PrismaService を使うためインポート
+import { UsersController } from './users.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [
-    PrismaModule, // UsersService が PrismaService を DI するために必要
-                 // もし PrismaModule に @Global() を付けていれば、厳密にはここでの import は不要ですが、
-                 // 依存関係を明確にするために記述しておくのが一般的です。
-  ],
-  // もし '/users' のようなエンドポイントを作る場合はここに UsersController を追加します
-  // controllers: [UsersController],
-  providers: [
-    UsersService, // UsersService をこのモジュールのプロバイダーとして登録
-  ],
-  exports: [
-    UsersService, // ★ UsersService をエクスポートして、AuthModule など他のモジュールから注入できるようにする
-  ]
+  imports: [PrismaModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
